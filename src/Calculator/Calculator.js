17d47d35ab98af1e6507bd5b7d902589cab78c52
Alexander <Alexander_alatorre1993@hotmail.com>
2022-09-19 17:11:28 -0600

    added some changes'

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,43 +1,46 @@
-import React, { useState } from "react";
+import React from 'react';
 
 export default function TipCalculator() {
   // Write your code here.
-  const [bill, setBill] = useState("50");
-  const [tip, setTip] = useState("18");
-  const [numberOfPeople, setNumberOfPeople] = useState("1");
+  const [bill, setBill] = useState(50);
+  const [tip, setTip] = useState(18);
+  const [numberOfPeople, setNumberOfPeople] = useState(1);
 
   const tipPercentage = parseInt(tip) / 100;
   const totalTip = (parseInt(bill) * tipPercentage).toFixed(2) || "-";
   const amountToPayPerPerson = totalTip / numberOfPeople;
-
+  
   return (
     <>
       {/* Write your code here. */}
-      <div>
+        <div>
         <label for="bill">Bill</label>
         <input
           id="bill"
-          type="text"
+          type="number"
           value={bill}
-          onChange={(e) => setBill(e.target.value)}
+          min="0"
+          onChange={(e) => setBill(parseInt(e.target.value))}
         />
       </div>
       <div>
         <label for="tip">Tip</label>
         <input
           id="tip"
-          type="text"
+          type="number"
+          min="0"
           value={tip}
-          onChange={(e) => setTip(e.target.value)}
+          onChange={(e) => setTip(parseInt(e.target.value))}
         />
       </div>
       <div>
         <label for="people">Number of People</label>
         <input
           id="people"
-          type="text"
+          type="number"
+          min="0"
           value={numberOfPeople}
-          onChange={(e) => setNumberOfPeople(e.target.value)}
+          onChange={(e) => setNumberOfPeople(parseInt(e.target.value))}
         />
       </div>
       <div>
