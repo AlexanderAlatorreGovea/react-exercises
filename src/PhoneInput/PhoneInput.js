cffc615bcb9916eb3c48b503e6972cd43bb40bfc
Alexander <Alexander_alatorre1993@hotmail.com>
2022-09-24 17:27:16 -0600

    solved phone input

--- src/PhoneInput/PhoneInput.js
+++ src/PhoneInput/PhoneInput.js
@@ -10,19 +10,17 @@ export default function PhoneInput() {
   const hasExceededTheMaximumAmountOfCharsAllowed =
     value.length === MAXIMUM_ALLOWED_INPUT_CHARACTERS;
 
-  const formatInputValue = (value) => {
-    return value;
+  const onChange = (e) => {
+    setValue(formatInputValue(e.target.value));
   };
 
   return (
     <>
       <form>
         <input
-          type="text"
+          type="tel"
           value={formatInputValue(value)}
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
+          onChange={onChange}
           placeholder={PLACEHOLDER_PHONE_NUMBER}
         />
         <button
@@ -33,4 +31,26 @@ export default function PhoneInput() {
       </form>
     </>
   );
+}
+
+function formatInputValue(str) {
+  const rawString = str.replace(/\D/g, "");
+  let output = "";
+
+  if (rawString.length > 0) {
+    output += "(";
+    output += rawString.substring(0, 3);
+  }
+
+  if (rawString.length > 3) {
+    output += ") ";
+    output += rawString.substring(3, 6);
+  }
+
+  if (rawString.length > 6) {
+    output += "-";
+    output += rawString.substring(6, 10);
+  }
+
+  return output;
 }
