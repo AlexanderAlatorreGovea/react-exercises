6df8a9adae6010f265a3c7d9ab7a1bc21e23d035
Alexander <Alexander_alatorre1993@hotmail.com>
2022-09-26 16:35:56 -0600

    added tic tac toe

---
+++ src/ConnectFour/ConnectFour.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ConnectFour() {
+  return (
+    <>
+      <div className="tile">
+        <div className="player player-1"></div>
+      </div>
+    </>
+  );
+}
