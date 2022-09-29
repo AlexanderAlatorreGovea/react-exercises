6e7ac6241c769d2db1ebc97b9926be93d02d3317
Alexander <Alexander_alatorre1993@hotmail.com>
2022-09-29 17:47:30 -0600

    commented out some code

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <Table />
+      {/* <Table /> */}
       {/* <TicTacToe /> */}
       {/* <Quiz />
       <table className="table">
