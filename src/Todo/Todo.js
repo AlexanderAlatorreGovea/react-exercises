c6913b15ea3d230294e1991af518992cc2b355c6
Alexander <Alexander_alatorre1993@hotmail.com>
2022-10-01 18:19:54 -0600

    descrucutred props

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,7 +1,85 @@
-export const Todo = () => {
-  return <div>
-      <form >
+import { useState } from "react";
 
+const useInputValue = (inputValue) => {
+  const [value, setValue] = useState(inputValue);
+
+  return {
+    value,
+    type: "text",
+    onChange: (e) => setValue(e.target.value),
+    resetValue: () => setValue(""),
+  };
+};
+
+export const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const { resetValue, ...text } = useInputValue("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    setTodos([
+      ...todos,
+      {
+        text: text.value,
+        completed: false,
+      },
+    ]);
+    resetValue("");
+  };
+
+  const toggleCompleted = (i) => {
+    setTodos(
+      todos.map((todo, k) =>
+        k === i
+          ? {
+              ...todo,
+              completed: !todo.completed,
+            }
+          : todo
+      )
+    );
+  };
+
+  return (
+    <div>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "300px",
+          margin: "0 auto",
+        }}
+        onSubmit={(e) => addTodo(e)}
+      >
+        <input {...text} />
       </form>
-  </div>;
-}; 
+      {todos.map(({ text, completed }, i) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              width: "3rem",
+              margin: "20px auto",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              onClick={() => toggleCompleted(i)}
+              style={{
+                marginRight: "20px",
+                textDecoration: completed ? "line-through" : "none",
+              }}
+            >
+              {text}
+            </div>
+            <div style={{ backgroundColor: "red", padding: "5px" }}>X</div>
+          </div>
+        ); 
+      })}
+      <button onClick={() => setTodos([])}>reset todo</button>
+    </div>
+  );
+};
